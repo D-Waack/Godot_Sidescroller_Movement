46fc2e59cb1652735b772b398073d0ed99b15675
D-Waack <danielmwaack@gmail.com>
2022-10-07 17:52:38 -0300

    Add double jump flag, remove excessive comments

--- Player/Player.gd
+++ Player/Player.gd
@@ -25,11 +25,15 @@ export(float) var MAX_FALL_SPEED = 200.0 # maximum speed at which player charact
 export(float) var JUMP_FORCE = 140.0 # force with which the player jumps, has to counter gravity for a little bit. The higher this is, the higher and floatier the jump will be
 export(float) var MIN_AIR_DISTANCE = 60.0 # minimum distance a jump will go through before it can be stopped by letting go of the jump button
 
+### Ability variables
+export var can_double_jump : bool = true # allows player character to double jump
+export var can_wall_jump : bool = true # allows player character to wall jump
+
 ### Physics Variables 
 # These are physics variables that are changed during runtime, not manually set
 var velocity : Vector2 = Vector2.ZERO # velocity at which player character is moving
 
-# State Machine Variables
+### State Machine Variables
 enum {IDLE, RUN, RISE, FALL} # states of the state machine
 var state = IDLE # current state at which player character is at
 var double_jumped : bool = false # flag for if the player has double jumped or not
@@ -52,39 +56,40 @@ func get_horizontal_input() -> float:
 # This function returns the current state of the state machine the player is in
 func get_current_state(horizontal_input):
 	if is_on_floor(): # player character is touching the ground
-		if horizontal_input != 0: # check if player character is moving
-			return RUN # running state
-		return IDLE # idle state (not moving)
-	else: # if player character is not touching the ground
+		if horizontal_input != 0:
+			return RUN 
+		return IDLE 
+	else: 
 		if velocity.y < 10: # if the player is rising, or just started falling
-			return RISE # rising state
+			return RISE 
 		else:
-			return FALL # falling state
+			return FALL 
 
 # This function will apply gravity and other forces (like jump force) to the player character
 func vertical_movement():
-	velocity.y += GRAVITY # gravity is added to the current velocity
-	velocity.y = min(velocity.y, MAX_FALL_SPEED) # velocity's Y value cannot go past max fall speed
-	if is_on_floor(): # if player character is on the floor, reset double jump and check for jump input
+	velocity.y += GRAVITY 
+	velocity.y = min(velocity.y, MAX_FALL_SPEED) 
+	if is_on_floor(): 
 		double_jumped = false
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = -JUMP_FORCE # when the jump button is pressed, jump force is subtracted from velocity's Y value
-	else: # player is not on floor, therefore falling or rising
-		if Input.is_action_just_released("ui_up") and velocity.y < -MIN_AIR_DISTANCE: # if the jump button is released and player has already raised the minimum distance
+			velocity.y = -JUMP_FORCE 
+	else: 
+		if Input.is_action_just_released("ui_up") and velocity.y < -MIN_AIR_DISTANCE: 
 			velocity.y = -MIN_AIR_DISTANCE # cut velocity's Y value down so the player stops when letting go of the jump button
-		if not double_jumped: # if player is not on the ground and has not yet double jumped, check for double jump input
+		if not double_jumped and can_double_jump: 
 			if Input.is_action_just_pressed("ui_up"):
-				double_jumped = true # double_jumped flag is set to true, so player cannot double jump again
-				velocity.y = -JUMP_FORCE # jump again
+				double_jumped = true 
+				velocity.y = -JUMP_FORCE
 
 # This function is responsible for moving the player character through space according to the input
+# When there is no input, apply friction, when there is, apply acceleration
 func horizontal_movement(horizontal_input):
-	if horizontal_input == 0: # when player character is not moving, apply friction
+	if horizontal_input == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
-	else: # when player character is moving, apply acceleration
+	else:
 		velocity.x = move_toward(velocity.x, horizontal_input * MAX_SPEED, ACCELERATION)
-	velocity = move_and_slide(velocity, UP_DIRECTION) # function that actually moves the player based on velocity. 
-	# obs: up direction is needed for "is_on_floor()" and "is_on_wall()" to work
+	velocity = move_and_slide(velocity, UP_DIRECTION) # function that actually moves the player based on velocity
+	# !!! up direction is needed for "is_on_floor()" and "is_on_wall()" to work
 
 # This function will display the correct animation for the player character based on its state and horizontal input
 func animation(horizontal_input):
@@ -92,19 +97,19 @@ func animation(horizontal_input):
 		IDLE: 
 			animator.travel("idle")
 		RUN:
-			if horizontal_input > 0: # when facing left, the player character's sprite must be flipped
+			if horizontal_input > 0: 
 				flip_character(false)
 			else:
 				flip_character(true)
 			animator.travel("run")
 		RISE:
-			if horizontal_input > 0: # when facing left, the player character's sprite must be flipped
+			if horizontal_input > 0: 
 				flip_character(false)
 			elif horizontal_input < 0:
 				flip_character(true)
 			animator.travel("rise")
 		FALL:
-			if horizontal_input > 0: # when facing left, the player character's sprite must be flipped
+			if horizontal_input > 0: 
 				flip_character(false)
 			elif horizontal_input < 0:
 				flip_character(true)
