c8f540c56eddb76ce48d6b7107601a2d245f2733
D-Waack <danielmwaack@gmail.com>
2022-10-07 18:06:48 -0300

    Add comments to animation tree script

--- Player/AnimationTree.gd
+++ Player/AnimationTree.gd
@@ -2,8 +2,10 @@ extends AnimationTree
 
 var animator = self["parameters/playback"]
 
+# Returns the name of the current animation
 func get_current():
 	return animator.get_current_node()
 
+# Starts the next animation 
 func travel(animation : String):
 	animator.travel(animation)
