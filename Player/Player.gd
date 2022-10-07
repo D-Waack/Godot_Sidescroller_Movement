42ebd25daa8d4d94f47ddb7a0e85f3b35a2d828f
D-Waack <danielmwaack@gmail.com>
2022-10-07 19:56:59 -0300

    Add delta time and adjust variables

--- Player/Player.gd
+++ Player/Player.gd
@@ -17,17 +17,18 @@ const UP_DIRECTION : Vector2 = Vector2.UP # needed for move_and_slide function
 # They are exported so you can easily tweak them on the editor
 ## Horizontal speed variables
 export(float) var MAX_SPEED = 120.0 # maximum speed at which the player character can move
-export(float) var ACCELERATION = 10.0 # rate at which player character's speed rises
-export(float) var FRICTION = 30.0 # rate at which player character's speed lowers
+export(float) var ACCELERATION = 600.0 # rate at which player character's speed rises
+export(float) var FRICTION = 1200.0 # rate at which player character's speed lowers
 ## Vertical speed variables
-export(float) var GRAVITY = 4.0 # force that pulls player character down, also known as gravity
+export(float) var GRAVITY = 240.0 # force that pulls player character down, also known as gravity
 export(float) var MAX_FALL_SPEED = 200.0 # maximum speed at which player character can fall
 export(float) var JUMP_FORCE = 140.0 # force with which the player jumps, has to counter gravity for a little bit. The higher this is, the higher and floatier the jump will be
 export(float) var MIN_AIR_DISTANCE = 60.0 # minimum distance a jump will go through before it can be stopped by letting go of the jump button
+## Other speed variables
+#export(float) var MIN_WALL_JUMP_DISTANCE = 30.0 # 
 
 ### Ability variables
 export var can_double_jump : bool = true # allows player character to double jump
-export var can_wall_jump : bool = true # allows player character to wall jump
 
 ### Physics Variables 
 # These are physics variables that are changed during runtime, not manually set
@@ -40,11 +41,11 @@ var double_jumped : bool = false # flag for if the player has double jumped or n
 
 ### Functions that run every frame
 # Physics process, main loop separated into subroutines
-func _physics_process(_delta):
+func _physics_process(delta):
 	var horizontal_input : float = get_horizontal_input() 
 	state = get_current_state(horizontal_input)
-	vertical_movement()
-	horizontal_movement(horizontal_input)
+	vertical_movement(delta)
+	horizontal_movement(horizontal_input, delta)
 	animation(horizontal_input)
 
 ### State machine routines
@@ -66,8 +67,8 @@ func get_current_state(horizontal_input):
 			return FALL 
 
 # This function will apply gravity and other forces (like jump force) to the player character
-func vertical_movement():
-	velocity.y += GRAVITY 
+func vertical_movement(delta):
+	velocity.y += GRAVITY * delta
 	velocity.y = min(velocity.y, MAX_FALL_SPEED) 
 	if is_on_floor(): 
 		double_jumped = false
@@ -83,11 +84,11 @@ func vertical_movement():
 
 # This function is responsible for moving the player character through space according to the input
 # When there is no input, apply friction, when there is, apply acceleration
-func horizontal_movement(horizontal_input):
+func horizontal_movement(horizontal_input, delta):
 	if horizontal_input == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, horizontal_input * MAX_SPEED, ACCELERATION)
+		velocity.x = move_toward(velocity.x, horizontal_input * MAX_SPEED, ACCELERATION * delta)
 	velocity = move_and_slide(velocity, UP_DIRECTION) # function that actually moves the player based on velocity
 	# !!! up direction is needed for "is_on_floor()" and "is_on_wall()" to work
 
