49ae02e138d24f87b394a7f65d8995a14bf0862d
D-Waack <danielmwaack@gmail.com>
2022-10-09 13:14:59 -0300

    Fix fonts folder location

--- Player/DeathOverlay.gd
+++ Player/DeathOverlay.gd
@@ -8,7 +8,9 @@ func _ready():
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().reload_current_scene()
+		var error_message = get_tree().reload_current_scene()
+		if error_message != 0:
+			print(error_message)
 
 func display_death_panel():
 	panel.set_visible(true)
